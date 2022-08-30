5e11f252e8b6d471502539f0c255de12fd62522c
EleazarSF <eleazarsf@gmail.com>
2022-08-30 07:16:29 -0300

    routes fixed

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('api/futurama/characters', [ApiController::class, 'futuramaCharactersAPI'])->name('futuramaCharactersAPI');
-Route::get('api/futurama/character/{id}', [ApiController::class, 'futuramaSingleCharacterAPI'])->name('futuramaSingleCharacterAPI');
+Route::get('characters', [ApiController::class, 'futuramaCharactersAPI'])->name('futuramaCharactersAPI');
+Route::get('character/{id}', [ApiController::class, 'futuramaSingleCharacterAPI'])->name('futuramaSingleCharacterAPI');
