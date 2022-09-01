626ef241d605757f6882107e9c40e46d75f2fef5
EleazarSF <eleazarsf@gmail.com>
2022-09-01 07:06:57 -0300

    copy checker added to copy button

--- assets/js/copyEndpoint.js
+++ assets/js/copyEndpoint.js
@@ -1,12 +1,12 @@
 function copyCharactersEndpoint() {  
     var copyEndpoint = document.getElementById("charactersEndpoint");
     navigator.clipboard.writeText(copyEndpoint.value);  
-    copyEndpoint.innerHTML = "Copied";
+    copyEndpoint.innerHTML = `<i class="fa-solid fa-check"></i> Copied`;
   };
   function copySingleCharacterEndpoint() {  
     var copyEndpoint = document.getElementById("singleCharacterEndpoint");
     navigator.clipboard.writeText(copyEndpoint.value);  
-    copyEndpoint.innerHTML = "Copied";
+    copyEndpoint.innerHTML = `<i class="fa-solid fa-check"></i> Copied`;
   }
   function display () {
     var display = document.querySelector("#display");
