fa91f8756f0668d73bac9c8d463d7996cf7e8196
EleazarSF <eleazarsf@gmail.com>
2022-09-01 14:29:12 -0300

    route root fixed

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect("https://eleazarfreitas.github.io/futurama-API/");
 });
 
 Route::get('characters', [ApiController::class, 'futuramaCharactersAPI'])->name('futuramaCharactersAPI');
